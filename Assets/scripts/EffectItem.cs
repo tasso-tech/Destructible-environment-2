22711711f005dde9b3a650e736cce3bf94823d3c
26689 <tasso.mevissen@gmail.com>
2020-09-28 10:46:21 +0200

    Inventory 1
    added scripts for a inventory systems

---
+++ Assets/scripts/EffectItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectItem : MonoBehaviour
+{
+    public GameObject effect;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void Use()
+    {
+        Instantiate(effect, player.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
